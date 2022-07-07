0dc0546ce88dc2b500bc0f27419b0ec96a628ada
chiachun wu <a0283137@gmail.com>
2022-07-07 09:14:51 +0800

    作業資料夾github路徑更新

--- Upload/del_file.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "base.php";
-$id=$_GET['id'];
-$file=$pdo->query("SELECT * FROM `upload` WHERE `id`='$id'")->fetch();
-unlink("./upload/".$file['name']);
-del($id);
-header("location:manage.php");
-?>
